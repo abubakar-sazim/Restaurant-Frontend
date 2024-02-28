4cb1cf374a3418a1598a2cd6ab54285542d35a0e
abubakar-sazim <abubakar.siddik@sazim.io>
2024-02-28 20:42:23 +0600

    bage and tooltip

--- dishdive/components/DetailPage/Attributes/Attributes.tsx
+++ dishdive/components/DetailPage/Attributes/Attributes.tsx
@@ -5,6 +5,13 @@ import {
   getWifiBooleanIcon,
 } from "./Attributes.helper";
 import { RestaurantData } from "../Helper/helper";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Badge } from "@/components/ui/badge";
 
 const AttributeTable = (restaurantData: RestaurantData) => {
   const {
@@ -20,6 +27,8 @@ const AttributeTable = (restaurantData: RestaurantData) => {
   } = restaurantData;
   const wifi = wiFi === "o" ? "no" : wiFi === "ree" ? "free" : wiFi;
 
+  console.log(businessAcceptsCreditCards);
+
   return (
     <div className="p-4 mt-2">
       <div className="flex items-center">
@@ -28,80 +37,138 @@ const AttributeTable = (restaurantData: RestaurantData) => {
         </div>
         <h1 className="ml-2 font-bold text-lg">Attributes</h1>
       </div>
-      <table className="min-w-full divide-y divide-gray-200 mt-4">
-        <tbody className="bg-white divide-y divide-gray-200">
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Alcohol</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">{alcohol}</td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Wifi</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getWifiBooleanIcon(wifi)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Accepts Credit Cards</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(businessAcceptsCreditCards)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Good for Kids</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(goodForKids)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Has TV</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(hasTV)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Noise Level</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getNoiseLevelIcon(noiseLevel)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Outdoor Seating</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(outdoorSeating)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Delivery</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(restaurantsDelivery)}
-            </td>
-          </tr>
-          <tr>
-            <td className="px-3 py-2 whitespace-nowrap">
-              <strong>Good for Groups</strong>
-            </td>
-            <td className="px-3 py-2 whitespace-nowrap">
-              {getBooleanIcon(restaurantsGoodForGroups)}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <TooltipProvider>
+        <table className="min-w-full divide-y divide-gray-200 mt-4">
+          <tbody className="bg-white divide-y divide-gray-200">
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Alcohol</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Badge variant="outline">{alcohol}</Badge>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Wifi</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>{getWifiBooleanIcon(wifi)}</TooltipTrigger>
+                  <TooltipContent>
+                    <p>{wifi}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Accepts Credit Cards</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>
+                    {getBooleanIcon(businessAcceptsCreditCards)}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>
+                      {businessAcceptsCreditCards ? <p>True</p> : <p>False</p>}
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Good for Kids</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>{getBooleanIcon(goodForKids)}</TooltipTrigger>
+                  <TooltipContent>
+                    <p>{goodForKids ? <p>True</p> : <p>False</p>}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>TV</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>{getBooleanIcon(hasTV)}</TooltipTrigger>
+                  <TooltipContent>
+                    <p>{hasTV ? <p>Yes</p> : <p>No</p>}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Noise Level</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>
+                    {getNoiseLevelIcon(noiseLevel)}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{noiseLevel}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Outdoor Seating</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>
+                    {getBooleanIcon(outdoorSeating)}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{outdoorSeating ? <p>True</p> : <p>False</p>}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Delivery</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>
+                    {getBooleanIcon(restaurantsDelivery)}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{restaurantsDelivery ? <p>Yes</p> : <p>No</p>}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <strong>Good for Groups</strong>
+              </td>
+              <td className="px-3 py-2 whitespace-nowrap">
+                <Tooltip>
+                  <TooltipTrigger>
+                    {getBooleanIcon(restaurantsGoodForGroups)}
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>
+                      {restaurantsGoodForGroups ? <p>True</p> : <p>False</p>}
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </TooltipProvider>
     </div>
   );
 };
